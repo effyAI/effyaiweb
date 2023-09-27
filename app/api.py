c61f85651f4d8fe0719f43a0bb25611de8a2a067
SatyaDewangan05 <satyadewangan05@gmail.com>
2023-09-27 17:30:46 +0530

    Implemented new modification of ec2 to old stable commit from the windows

--- app/api.py
+++ app/api.py
@@ -10,23 +10,39 @@ import boto3
 from database import db
 from models import CorpusData
 
+import os
+import pprint
+import torch
+import torchvision.transforms as transforms
+from argparse import Namespace
+from .models.psp import pSp
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'the quick brown fox jumps over the lazy   dog'
-app.config['CORS_HEADERS'] = 'Content-Type'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///corpus_data.db'
-db.init_app(app)
 api = Api(app)
-# Configure CORS using the imported function
-configure_cors(app)
-# Create the tables (run only once when the app starts)
-with app.app_context():
-    db.create_all()
+
+# Model Loading
+model_path = "effyaiweb/app/src/pretrained_models/sam_ffhq_aging.pt"
+if not os.path.exists(model_path):
+    print('Download pretained model and save it in "app/src/pretained_models" dir or if already downloaded then save the model path correctly')
+ckpt = torch.load(model_path, map_location='cpu')
+opts = ckpt['opts']
+pprint.pprint(opts)
+opts['checkpoint_path'] = model_path
+opts = Namespace(**opts)
+net = pSp(opts)
+
+
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
 @app.route('/hello')
 def hello():
     return {"response": "Hello"}
+
+
 @app.route('/calculate_sip', methods=['POST'])
 def calculate_sip():
     currentAge = int(request.form['CurrentAge'])
@@ -60,7 +76,8 @@ def calculate_sip():
         return f'Error: {e}'
     monthlyinterestRate = interestRate / 12
     numberofMonths = (retirementAge - currentAge) * 12
-    MonthlySIP = (corpusGoal * monthlyinterestRate) / ((1 + monthlyinterestRate) ** numberofMonths - 1)
+    MonthlySIP = (corpusGoal * monthlyinterestRate) / \
+        ((1 + monthlyinterestRate) ** numberofMonths - 1)
     # MonthlySIP = (corpusGoal * monthlyinterestRate) / (math.pow(1 + monthlyinterestRate, numberofMonths) - 1)
     # Initialize a list to store closing balances for each year
     closingBalances = []
@@ -98,6 +115,8 @@ def calculate_sip():
         'id': corpus_data.id
     }
     return jsonify(response_data)
+
+
 @app.route('/fetch_corpus_data/<int:id>', methods=['GET'])
 def fetch_corpus_data_by_id(id):
     corpus_data = CorpusData.query.get(id)
@@ -115,14 +134,24 @@ def fetch_corpus_data_by_id(id):
     }
     return jsonify(data)
 
-@app.route('/get_aging_video', methods=['GET'])
+
+@app.route('/get_aging_video', methods=['POST'])
 def get_aging_video():
-    # data = request.get_json()
-    input_img = 'https://i.pinimg.com/736x/ad/b4/60/adb4602cbd454354e71b5f214fb4e0ec.jpg'
-    currentAge = 25
-    retirementAge = 80
-    res = age_input(input_img, currentAge, retirementAge)
-    return res
+    data = request.get_json()
+
+    s3_image_path = data.request('s3_image_path')
+    current_age = data.request('current_age')
+    retirement_age = data.request('retirement_age')
+    # input_img = 'https://i.pinimg.com/736x/ad/b4/60/adb4602cbd454354e71b5f214fb4e0ec.jpg'
+    # currentAge = 25
+    # retirementAge = 80
+    try:
+        res = age_input(s3_image_path, current_age, retirement_age)
+        return {'output_path': res}
+    except:
+        return {'error': 404}
+    # return res
+
 
 if __name__ == "__main__":
     app.run()
--- app/src/get_aging_video.py
+++ app/src/get_aging_video.py
@@ -1,3 +1,11 @@
+import uuid
+import subprocess
+import ffmpeg
+import cv2
+from .database import upload_image_to_s3
+from .models.psp import pSp
+from .utils.common import tensor2im
+from .datasets.augmentations import AgeTransformer
 from argparse import Namespace
 import os
 import sys
@@ -11,15 +19,11 @@ from io import BytesIO
 sys.path.append(".")
 sys.path.append("..")
 # sys.path.append('/home/ubuntu/effyaiweb/src')
-from .datasets.augmentations import AgeTransformer
-from .utils.common import tensor2im
-from .models.psp import pSp
-from .database import upload_image_to_s3
 
 # Video Conversion
-import cv2
 
-def age_input(input1, input3, input4):
+
+def age_input(input1, input2, input3, input4):
     # if len(sys.argv) != 5:
     #     print(f'current len of agruments: {len(sys.argv)}')
     #     print("Usage: python script.py Input_path Output_Dir Current_Age Require_Age")
@@ -30,7 +34,7 @@ def age_input(input1, input3, input4):
     # arg4 = int(sys.argv[4]) # Required Age
 
     arg1 = input1
-    # arg2 = input2
+    net = input2
     arg3 = input3
     arg4 = input4
 
@@ -51,34 +55,38 @@ def age_input(input1, input3, input4):
     EXPERIMENT_ARGS = EXPERIMENT_DATA_ARGS[EXPERIMENT_TYPE]
     model_path = EXPERIMENT_ARGS['model_path']
 
-    if not os.path.exists(model_path):
-        print('Download pretained model and save it in "app/src/pretained_models" dir or if already downloaded then save the model path correctly')
-        return 'null'
+    # if not os.path.exists(model_path):
+    #     print('Download pretained model and save it in "app/src/pretained_models" dir or if already downloaded then save the model path correctly')
+    #     return 'null'
+    # ckpt = torch.load(model_path, map_location='cpu')
+    # opts = ckpt['opts']
+    # pprint.pprint(opts)
+    # opts['checkpoint_path'] = model_path
+    # opts = Namespace(**opts)
+    # net = pSp(opts)
 
-    ckpt = torch.load(model_path, map_location='cpu')
-    opts = ckpt['opts']
-    pprint.pprint(opts)
-
-    opts['checkpoint_path'] = model_path
-    opts = Namespace(**opts)
-    net = pSp(opts)
     net.eval()
     net.cuda()
     print('Model successfully loaded!')
 
     # Download Image from URL
-    input_url = arg1 # Input Image
-    response = requests.get(input_url)
+    input_url = arg1  # Input Image
+    try:
+        response = requests.get(input_url)
+    except:
+        return {'error': 404}
     if response.status_code == 200:
         # Open the image using PIL (Python Imaging Library)
-        img = Image.open(BytesIO(response.content)).convert("RGB") 
+        img = Image.open(BytesIO(response.content)).convert("RGB")
         # Save the image locally
         img.save('/home/ubuntu/effyaiweb/app/src/input/downloaded_image.jpg')
         print("Image downloaded and saved as 'downloaded_image.jpg'")
     else:
         print("Failed to download the image. Status code:", response.status_code)
+        return {'error': 404}
 
-    original_image = Image.open('effyaiweb/app/src/input/downloaded_image.jpg').convert("RGB") # Input URL
+    original_image = Image.open(
+        'effyaiweb/app/src/input/downloaded_image.jpg').convert("RGB")  # Input URL
     # image_path = arg1 # Input Path
     # original_image = Image.open(image_path).convert("RGB") # Input Path
     original_image.resize((256, 256))
@@ -86,12 +94,19 @@ def age_input(input1, input3, input4):
     def run_alignment(image_path):
         import dlib
         from .scripts.align_all_parallel import align_face
-        predictor = dlib.shape_predictor("effyaiweb/app/src/shape_predictor_68_face_landmarks.dat")
+        predictor = dlib.shape_predictor(
+            "effyaiweb/app/src/shape_predictor_68_face_landmarks.dat")
         aligned_image = align_face(filepath=image_path, predictor=predictor)
+        if isinstance(aligned_image, str) and aligned_image == 'No Face Found':
+            print('No Face Found')
+            return 'No Face Found'
         print("Aligned image has shape: {}".format(aligned_image.size))
         return aligned_image
 
-    aligned_image = run_alignment('effyaiweb/app/src/input/downloaded_image.jpg') # Input URL
+    aligned_image = run_alignment(
+        'effyaiweb/app/src/input/downloaded_image.jpg')  # Input URL
+    if isinstance(aligned_image, str) and aligned_image == 'No Face Found':
+        return {'error': 405}
     # aligned_image = run_alignment(image_path) # Input Path
     aligned_image.resize((256, 256))
     img_transforms = EXPERIMENT_ARGS['transform']
@@ -99,11 +114,11 @@ def age_input(input1, input3, input4):
 
     # Image Generation
     def run_on_batch(inputs, net):
-        result_batch = net(inputs.to("cuda").float(), randomize_noise=False, resize=False)
+        result_batch = net(inputs.to("cuda").float(),
+                           randomize_noise=False, resize=False)
         return result_batch
 
-
-    # we'll run the image on multiple target ages 
+    # we'll run the image on multiple target ages
     target_ages = [i for i in range(arg3, arg4 + 1)]
     age_transformers = [AgeTransformer(target_age=age) for age in target_ages]
 
@@ -115,10 +130,11 @@ def age_input(input1, input3, input4):
     # Video Options
     # image_folder = '/home/ubuntu/stylegan/sam_ready/SAM/output'
     # image_list = os.listdir(image_folder)
-    video_name = f"{os.getcwd()}/effyaiweb/app/src/output_video/{arg1.split('.')[0].split('/')[-1]}.mp4"
+    video_name = f"{os.getcwd()}/effyaiweb/app/src/output_video/generated_output.mp4"
     fps = 6  # You can adjust the frame rate as needed.
     frame_size = (1024, 1024)  # Set the width and height of the frames.
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # You can change the codec as needed.
+    # You can change the codec as needed.
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
     out = cv2.VideoWriter(video_name, fourcc, fps, frame_size)
 
     for age_transformer in age_transformers:
@@ -128,11 +144,11 @@ def age_input(input1, input3, input4):
             input_image_age = torch.stack(input_image_age)
             result_tensor = run_on_batch(input_image_age, net)[0]
             result_image = tensor2im(result_tensor)
-            results = np.concatenate([results, result_image], axis=1)
+            # results = np.concatenate([results, result_image], axis=1)
 
-            result_images.append(np.array(result_image)) # List of Image Array
+            # result_images.append(np.array(result_image))  # List of Image Array
 
-            result_image = Image.fromarray(np.array(result_image))
+            # result_image = Image.fromarray(np.array(result_image))
             # result_image.save(f"{arg2}/{count}.jpg") # save image at full resolution
 
             # image = cv2.imdecode(np.array(result_image), flags=cv2.IMREAD_COLOR)
@@ -143,11 +159,26 @@ def age_input(input1, input3, input4):
     out.release()
     cv2.destroyAllWindows()
 
-    s3_vid_name = f"{arg1.split('.')[0].split('/')[-1]}.mp4"
+    # s3_vid_name = f"{arg1.split('.')[0].split('/')[-1]}.mp4"
+
+    # mp4 to h264 conversion
+    input_file = video_name  # Replace with the path to your input MP4 file
+    # Output h264 path
+    output_file = f'{os.getcwd()}/effyaiweb/app/src/output_video/output.mp4'
+    command = ['ffmpeg', '-i', input_file, '-c:v', 'libx264',
+               output_file]  # Run FFmpeg command using subprocess
+    subprocess.call(command)
+
+    # UUID generation
+    uuid1 = uuid.uuid1()
+    s3_vid_name = f'{uuid1}.mp4'
 
     # upload to s3
-    res_url = upload_image_to_s3('AKIAVZBVXJWJLAWNRCWZ', 'SzjAgZQBhe7oPaQfqNgkWAe34aAHnBrd9CD1Kbjx', 'us-east-1', 'effy-bandhan', s3_vid_name, video_name)
+    res_url = upload_image_to_s3('AKIAVZBVXJWJLAWNRCWZ', 'SzjAgZQBhe7oPaQfqNgkWAe34aAHnBrd9CD1Kbjx',
+                                 'ap-southeast-1', 'effy-bandhan', s3_vid_name, output_file)
 
+    os.unlink('/home/ubuntu/effyaiweb/app/src/input/downloaded_image.jpg')
     os.unlink(video_name)
+    os.unlink(output_file)
 
     return res_url
